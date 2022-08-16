7fae30610019a009862f2c89e371cb52a38f357c
Boris <boris.bortnitskii@yandex.by>
2022-08-16 21:27:39 +0400

    init

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,21 +1,35 @@
 class GuessingGame {
-    constructor() {}
+    constructor(number) {
+        this.answer = number
+        this.solution = 0
+    }
 
     setRange(min, max) {
-
+        this.min = min
+        this.max = max
     }
 
     guess() {
+        this.solution = Math.round((this.min + this.max) / 2)
 
+        if (this.solution < this.answer) {
+            greater()
+        } else if (this.solution > this.answer) {
+            lower()
+        } else {
+            return this.solution
+        }
     }
 
     lower() {
-
+        this.max = this.solution
+        this.guess()
     }
 
     greater() {
-
+        this.min = this.solution
+        this.guess()
     }
 }
 
-module.exports = GuessingGame;
+module.exports = GuessingGame
